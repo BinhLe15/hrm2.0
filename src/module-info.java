cc3186e29b242ff5dc0a0c3eb50bf1f5f159fea7
BinhLe15 <110914485+BinhLe15@users.noreply.github.com>
2024-05-10 22:04:25 +0700

    skip commit

---
+++ src/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ *
+ */
+module testGraphic {
+	requires java.desktop;
+//	requires com.twelvemonkeys.common.image;
+	requires TimingFramework;
+	requires java.sql;
+	requires java.xml;
+	requires poi.ooxml;
+	requires poi;
+	requires java.mail;
+}
